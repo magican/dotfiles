4da2a71f6984da4255d6ed08f8274379c41d6d47
alexander.myasoedov <alexander.myasoedov@gmail.com>
2020-05-21 13:01:47 +0300

    Installing TeX recommended fonts and updating.

--- pandoc/install.sh
+++ pandoc/install.sh
@@ -15,21 +15,26 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-sudo tlmgr install collection-fontsrecommended
 sudo tlmgr update --all --self
+
+sudo tlmgr install titlesec
+sudo tlmgr install framed
+sudo tlmgr install threeparttable
+sudo tlmgr install wrapfig
+sudo tlmgr install multirow
+sudo tlmgr install enumitem
+sudo tlmgr install bbding
+sudo tlmgr install titling
+sudo tlmgr install tabu
+sudo tlmgr install mdframed
 sudo tlmgr install tcolorbox
-sudo tlmgr install pgf
-sudo tlmgr install xcolor
+sudo tlmgr install textpos
+sudo tlmgr install import
+sudo tlmgr install varwidth
+sudo tlmgr install needspace
+sudo tlmgr install tocloft
+sudo tlmgr install ntheorem
 sudo tlmgr install environ
 sudo tlmgr install trimspaces
-sudo tlmgr install mathpazo
-sudo tlmgr install parskip
-sudo tlmgr install adjustbox
-sudo tlmgr install collectbox
-sudo tlmgr install eurosym
-sudo tlmgr install ecs
-sudo tlmgr install ucs
-sudo tlmgr install enumitem
-sudo tlmgr install ulem
-sudo tlmgr install mathrsfs
-sudo tlmgr install jknapltx rsfs
+
+sudo tlmgr install collection-fontsrecommended
