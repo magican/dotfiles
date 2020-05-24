29f38aabd0eb21fba74051204fc01fc67229a0f8
magican <alexander.myasoedov@gmail.com>
2020-05-24 10:35:13 +0300

    [WIP] Moving to simplier bash scripts

--- pandoc/install.sh
+++ pandoc/install.sh
@@ -9,12 +9,6 @@ fi
 
 printf "› Installing TeX recommended fonts and updating.\\n"
 
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 sudo tlmgr update --all --self
 
 sudo tlmgr install titlesec
